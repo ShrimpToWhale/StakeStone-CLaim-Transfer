c1a0f852cc3b97f99c96e72adbd277898cad1ee5
Maxxiitto <drop.hunter.m@gmail.com>
2025-04-05 23:02:06 +0300

    First commit

---
+++ constants.py
@@ -0,0 +1,16 @@
+bsc_rpc_url = 'https://bsc.meowrpc.com'
+
+STO_address = '0xdAf1695c41327b61B9b9965Ac6A5843A3198cf07'
+
+claim_address = '0x04bB7043eBbe5EC3f6a08EC45b3De8C36e0628B3'
+
+bsc_explorer_url = 'https://bscscan.com/'
+
+claim_url = 'https://airdrop.stakestone.io/api/claim-data'
+
+headers = {
+    "Content-Type": "application/json",
+    "Origin": "https://airdrop.stakestone.io",
+    "Referer": "https://airdrop.stakestone.io/unified",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+}
